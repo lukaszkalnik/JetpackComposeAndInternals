23abf94c135e12ac474e3fbaf735039636a0f628
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2024-01-24 20:32:40 +0100

    Exercise 4: LazySpeakersScreen

--- app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/LazySpeakersScreen.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/LazySpeakersScreen.kt
@@ -14,16 +14,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import dev.jorgecastillo.compose.app.R
+import androidx.compose.ui.unit.dp
 import dev.jorgecastillo.compose.app.data.FakeSpeakerRepository
 import dev.jorgecastillo.compose.app.models.Speaker
 import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
 
 @Composable
 fun LazySpeakersScreen(speakers: List<Speaker>) {
-    
+    Scaffold(
+        topBar = {
+            TopAppBar {
+                Text(
+                    text = "Speakers",
+                    modifier = Modifier.padding(16.dp),
+                )
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(painter = rememberVectorPainter(image = Icons.Default.Add), contentDescription = null)
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(paddingValues)
+                .testTag("SpeakersList")
+        ) {
+            items(speakers) { speaker ->
+                SpeakerCard(speaker = speaker)
+            }
+        }
+    }
 }
 
 @Composable
